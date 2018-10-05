4989e26803cc69a8da58509afd5c4bf7461e0b65
ILdar Shamiev <shamief@yandex.ru>
2018-10-05 21:05:51 +0300

    w2 sol2 added stress tests

---
+++ solutions/w2/linprog_test.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy.optimize import linprog
+
+
+if __name__ == '__main__':
+    A = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
+    b = np.array([-59, 78, -34, 87])
+    c = np.array([100, -5])
+    linprog_res = linprog(-c, A_ub=A, b_ub=b, options={'tol': 1e-3})
+    result = 'Unknown'
+    if linprog_res.status == 3:
+        result = 'Infinity'
+    elif linprog_res.status == 2:
+        result = 'No solution'
+    else:
+        result = 'Bounded solution'
+        solution_x = linprog_res.x
+        print('x_ref =', ' '.join(list(map(lambda x: '%.18f' % float(x), solution_x))))
+    print(result)
