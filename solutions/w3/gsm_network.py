0fc614e10e42f1f0c3c6b4ebd39e0b028d19c599
ILdar Shamiev <shamief@yandex.ru>
2018-10-15 14:21:24 +0300

    w3sol1 added starter file

---
+++ solutions/w3/gsm_network.py
@@ -0,0 +1,20 @@
+# python3
+
+
+def print_equisatisfiable_sat_formula():
+    """
+    This solution prints a simple satisfiable formula
+    and passes about half of the tests.
+    Change this function to solve the problem.
+    :return:
+    """
+    print("3 2")
+    print("1 2 0")
+    print("-1 -2 0")
+    print("1 -2 0")
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    edges = [list(map(int, input().split())) for i in range(m)]
+    print_equisatisfiable_sat_formula()
