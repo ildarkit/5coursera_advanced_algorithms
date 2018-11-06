bdd0c1142cf3b5a1d35005e9be25a9fdfe14faf0
ILdar Shamiev <shamief@yandex.ru>
2018-11-06 14:27:17 +0300

    w4sol1 added starter file

---
+++ solutions/w4/circuit_design.py
@@ -0,0 +1,36 @@
+# python3
+
+
+def is_satisfiable():
+    """
+    This solution tries all possible 2^n variable assignments.
+    It is too slow to pass the problem.
+    Implement a more efficient algorithm here.
+    :return:
+    """
+    for mask in range(1<<n):
+        result = [(mask >> i) & 1 for i in range(n)]
+        formula_is_satisfied = True
+        for clause in clauses:
+            clause_is_satisfied = False
+            if result[abs(clause[0]) - 1] == (clause[0] < 0):
+                clause_is_satisfied = True
+            if result[abs(clause[1]) - 1] == (clause[1] < 0):
+                clause_is_satisfied = True
+            if not clause_is_satisfied:
+                formula_is_satisfied = False
+                break
+        if formula_is_satisfied:
+            return result
+    return None
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    clauses = [list(map(int, input().split())) for i in range(m)]
+    result = is_satisfiable()
+    if result is None:
+        print("UNSATISFIABLE")
+    else:
+        print("SATISFIABLE")
+        print(" ".join(str(-i-1 if result[i] else i+1) for i in range(n)))
